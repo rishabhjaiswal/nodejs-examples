4ece9b21471a3fc38363789c9378203ea961a525
rj1402 <rishabhjaiswal.1402@gmail.com>
2018-05-16 08:04:12 -0700

    node callbacks and error handling

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ var rect = require('./rectangle');
 function solveReactangle  (l, b) {
     console.log("Solveing for react with length : ", l);
     console.log("Solveing for react with breadth : ", b);
-    if ( l <= 0 || b <= 0 ){
-        console.log("invalid l or b")
-    } 
-    else {
-        console.log("area of reactangle is : ", rect.area(l,b));
-        console.log("perimeter of reactangle is : ", rect.perimeter(l,b));
-    }
+    rect (l, b, (err, rectangle) => {
+        if (err) {
+            console.log("Error: ",err.message);
+        } 
+        else {
+            console.log("area of reactangle : ", rectangle.area());
+            console.log("perimeter of reactangle : ", rectangle.perimeter());
+        }
+    });
+    console.log("thiss statement ius after rect call")
 }
 
 solveReactangle(2,3);
 solveReactangle(2,4);
 solveReactangle(0,5);
+solveReactangle(5,3);
