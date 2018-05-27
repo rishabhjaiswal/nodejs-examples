ec68d99adc353d05417022b7fd20b2c25269aed6
rj1402 <rishabhjaiswal.1402@gmail.com>
2018-05-27 14:21:52 -0700

    express example

--- index.js
+++ index.js
@@ -1,47 +1,21 @@
+const express = require('express');
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer ((req, res) => {
-    console.log("Requesr for " + req.url + " with method : " + req.method);
-    if (req.method === 'GET') {
-        var fileUrl;
-        if (req.url == '/') fileUrl = '/index.html';
-        else fileUrl = req.url;
+const app = express();
 
-        var filePath = path.resolve('./public'+ fileUrl);
-        const fileExt = path.extname(filePath);
-        if (fileExt == '.html') {
-            fs.exists(filePath, (exists) => {
-                if (!exists) {
-                    res.statusCode = 404;
-                    res.setHeader('Content-Type', 'text-html');
-                    res.end('<html><body><h1>not found!!!</h1></body></html>');
-                    return;
-                }
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text-html');
-                fs.createReadStream(filePath).pipe(res);
 
-            })
-        }
-        else {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text-html');
-            res.end('<html><body><h1>not an html file!!!</h1></body></html>');
-            return;
-        }
-    }
-    else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text-html');
-        res.end('<html><body><h1>not duppoprted!!!</h1></body></html>');
-        return;
-    }
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Contenr-Type','text/html');
+    res.end('<html><body><h1>this is an express server!!</h1></body></html>');
 });
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
     console.log(`Server runnning at http://${hostname}:${port}`);
